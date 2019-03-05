690db115a2a196f56709e82b6ba0020e1a53878d
Laura <laura.durackova@gmail.com>
2019-03-05 11:13:49 +0100

    Procedura na prienik a jej volanie

--- prvezadanie.c
+++ prvezadanie.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
 #include<math.h>
 
+void prienik(int A[6], int B[6])
+{
+	int i,j,C[6]={};
+	
+	for(i=0;i<6;i++)
+	{
+		for(j=0;j<6;j++)
+		{
+			if(A[i]==B[j])
+			{
+				C[i]=A[i];
+				printf("%d ",C[i]);
+			}
+		}
+	}
+}
+
 int main(void)
 {
 	int i,j,A[6]={3,9,4,8,7,1},B[6]={1,2,3,4,5,6},C[6]={},D[12]={};
@@ -18,4 +35,10 @@ int main(void)
 	{
 		printf("%d ",B[i]);
 	}
+	
+	printf("\n");
+	
+	printf("prienik mnozin: ");
+	prienik(A,B);
+	
 }

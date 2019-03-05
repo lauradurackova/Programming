020c457a5c5a496b39b6d53704fc299846877bb5
Laura <laura.durackova@gmail.com>
2019-03-05 11:12:53 +0100

    Inicializacia a vypis mnozin

---
+++ prvezadanie.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(void)
+{
+	int i,j,A[6]={3,9,4,8,7,1},B[6]={1,2,3,4,5,6},C[6]={},D[12]={};
+	
+	printf("mnozina A: ");
+	for(i=0;i<6;i++)
+	{
+		printf("%d ",A[i]);
+	}
+	
+	printf("\n");
+	
+	printf("mnozina B: ");
+	for(i=0;i<6;i++)
+	{
+		printf("%d ",B[i]);
+	}
+}

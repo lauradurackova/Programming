eeeb14cd0c02caff7b196b013b04310287a3275c
Laura <laura.durackova@gmail.com>
2019-05-07 17:40:21 +0200

    funkcia na prienik a jej volanie

--- matice.c
+++ matice.c
@@ -6,6 +6,20 @@ typedef struct{
 	unsigned int dlzka;
 }MNC;
 
+void prienik(MNC* a, MNC* b, MNC* c)
+{
+	int i,j;
+	
+	for(i=0;i<6;i++)
+	{
+		for(j=0;j<6;j++)
+		{
+			if(a->pole[i]==b->pole[j])
+				c->pole[i]=a->pole[i];
+		}
+	}
+}
+
 int main(void)
 {
 	int i,j,n=0,h=0;
@@ -31,4 +45,10 @@ int main(void)
 	{
 		printf("%d ",B[i]);
 	}
+	
+	printf("\n");
+	
+	printf("prienik mnozin: ");
+	prienik(&a,&b,&c);
+	
 }

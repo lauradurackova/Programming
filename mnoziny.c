677239392764f12322086d2c6d63d7b49237df41
lauradurackova <48187852+lauradurackova@users.noreply.github.com>
2019-05-10 09:32:43 +0200

    Struktura + main
    Zadefinovanie struktury MNC.
    Funkcia main s inicializovanymi mnozina a ich vypis.

--- mnoziny.c
+++ mnoziny.c
@@ -6,63 +6,7 @@ typedef struct{
 	unsigned int dlzka;
 }MNC;
 
-void prienik(MNC* a, MNC* b, MNC* c)
-{
-	int i,j;
-	
-	for(i=0;i<6;i++)
-	{
-		for(j=0;j<6;j++)
-		{
-			if(a->pole[i]==b->pole[j])
-				c->pole[i]=a->pole[i];
-		}
-	}
-}
 
-void zjednotenie(MNC* a, MNC* b, MNC* d)
-{
-	int i,j=0,n=0,h=0;
-	
-	for(i=0;i<6;i++)
-	{
-		while(d->pole[j]!=0)
-		{
-			if(a->pole[i]==d->pole[j])
-				n=1;
-
-			j++;
-		}
-		if(n==0)
-		{
-			d->pole[j]=a->pole[i];
-			h++;
-		}
-	}
-	
-	for(i=0;i<6;i++)
-	{
-		j=0;
-		n=0;
-		while(d->pole[j]!=0)
-		{
-			if(b->pole[i]==d->pole[j])
-				n=1;
-			
-			j++;
-		}
-		if(n==0)
-		{
-			d->pole[j]=b->pole[i];
-			h++;
-		}
-	}
-	
-//	for(i=0;i<h;i++)
-//	{
-//		printf("%d ",d->pole[i]);
-//	}
-}
 
 int main(void)
 {
@@ -90,14 +34,5 @@ int main(void)
 		printf("%d ",B[i]);
 	}
 	
-	printf("\n");
-	
-	printf("prienik mnozin: ");
-	prienik(&a,&b,&c);
-	
-	printf("\n");
-	
-	printf("zjednotenie mnozin: ");	
-	zjednotenie(&a,&b,&d);	
 	
 }
